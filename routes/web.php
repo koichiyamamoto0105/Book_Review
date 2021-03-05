7cc43a5c76ecf1e84596390f0ef0e1739d1e7f63
koichiyamamoto0105 <ko1y5osaka@gmail.com>
2021-03-05 09:52:44 +0000

    トップページの作成

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/' , 'ReviewController@index')->name('index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
